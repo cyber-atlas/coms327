520e2062c949154cbc5084aad401f6c8a29d39e1
Alex <ruski550@gmail.com>
2018-01-19 18:51:07 -0600

    Working on the random room generator, made the structs for the random room generator

--- Assignment1.01/generateRooms.c
+++ Assignment1.01/generateRooms.c
@@ -10,6 +10,7 @@ int roomCount=0;
 
 void border();
 void printer();
+void createRoom();
 
 int main(){
 
@@ -22,10 +23,51 @@ int main(){
 
 //Generates the start of the room
 void createRoom(){
+
 	
 	srand(time(NULL));
 
-	int xpos = 
+	struct Rooms {
+	
+		int x;
+		int y; 
+		int w; 
+		int h;
+	};
+
+ 	
+	for(int i =0; i <numRooms; i++){
+	
+	
+	}
+	
+
+	
+	//generates a random number between 5 and 10 for the number of rooms
+	int numRooms = rand() % 6 +5;
+
+	//TODO make and array with the randomly generated number of elements. Each element is room struct. 
+	//Need have a varaible that tracks the number of rooms your are using for looping over the array, just hard code 10;
+	//TODO structs don't need variables beacuase they are saved in array location.  
+	//Need to find a way to store xpos ypos xlength ylength	
+	//x needs length needs to be a minimum of 3 units, so start it away from the border, on the left and at least 3 units away on the right
+	int xpos = rand() % 75 + 1; 
+	int ypos = rand() % 19 + 1; 
+	
+	
+	//TODO find out max size for x and y lengths, just pick reasonable size for upper limits ont that stuff.
+	//Rooms must be at least 3 units long in the x direction
+	int xlen = rand() +3;
+	//Rooms must be at least 2 units long in the y direction
+	int ylen = rand() + 2;
+
+}
+
+
+void checker(int roomNum ){
+	if (dungeon[Rooms[roomNum].y][Rooms[roomnum].x] != '\0')
+
+
 
 }
 
@@ -45,7 +87,7 @@ void border(){
 }
 //Prints the dundeon to std out at the current location
 void printer(){
-	for (int i =0; i <= 21; i++){
+	for (int i =0; i  21; i++){
 		for (int j=0; j < 80; j++){
 			printf("%c", dungeon[i][j]);
 		}
@@ -53,3 +95,4 @@ void printer(){
 		printf("\n");
 	}
 }
+
