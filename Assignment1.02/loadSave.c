5853148bf8d019c39ce71a9cd82e539406282c86
Alex <ruski550@gmail.com>
2018-01-26 17:43:27 -0600

    Added the next project and started outlining how I am going to attack it

---
+++ Assignment1.02/loadSave.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+
+
+//Makes the directory. Ignores the error that occurs if it walready exists.
+int makeDirectory(){
+	mkdir ~/.
+}
+
+
+
+
+
+
+int main(){}
+
+
+
+
+//TODO check the home directory and make sure that there is the directory .rlg327. If not, create it
+//TODO if switch --save is called. Save the state HOW 
+//TODO two functions. fopen, gives you a file struct pointer which is an argument to (FILE) fwrite
+//
+/
+//
+//TODO make a rock array that is the size of the dungeon and generate all values to be larger than 0. Then go through and change all the hardness of rooms and corridors to 0. 
+//
+//TODO for the last part each room has 4 8 bits unsigned. 
